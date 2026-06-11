364ff422763bc7b67f5496e30126d16c5447096a
Aike05 <3149226872@qq.com>
2026-06-11 16:14:58 +0800

    fix: 安装 mysqli 扩展

--- simple.php
+++
@@ -1,5 +0,0 @@
-﻿<?php
-echo "<h1>Simple Test</h1>";
-echo "MYSQLHOST: " . (getenv("MYSQLHOST") ?: "(not set)") . "<br>";
-echo "PORT: " . (getenv("PORT") ?: "(not set)") . "<br>";
-echo "<br>PHP version: " . phpversion();
